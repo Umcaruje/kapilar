009c80b56aedce4480f87258f0ce90de112c0c4a
Umcaruje <uros.maravic@gmail.com>
2015-01-12 18:25:54 +0100

    Random text generator - Jquery magic

---
+++ static/js/textgen.js
@@ -0,0 +1,18 @@
+/* Simple Random Text generator */
+
+// Array with text
+var text = ['samoodrzivostznanjea', 'odrzivirazvojobrazov', 'istrajnostvernostrad', 'perspektivabuducnost'];
+
+// Random number (Thanks mozzila)
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$(document).ready(function () {
+	$('.clear').before(function () {
+		$(this).before(text[getRandomInt(0, 3)])
+	});
+	$('.clear').after(function () {
+		$(this).after(text[getRandomInt(0, 3)])
+	});
+});
